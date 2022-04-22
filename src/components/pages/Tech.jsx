ab175dad93d9c5be713359b6a59d560f8806ba4d
Lucas Viera <lucasviera22@gmail.com>
2022-04-22 16:49:26 -0300

    Modified tech stack

--- src/components/pages/Tech.jsx
+++ src/components/pages/Tech.jsx
@@ -4,70 +4,63 @@ import JsLogo from "../../assets/img/experience/icons8-javascript-240.png";
 import ReactLogo from "../../assets/img/experience/icons8-react-native-240.png";
 import NodeLogo from "../../assets/img/experience/icons8-node-js-240.png";
 import GitLogo from "../../assets/img/experience/icons8-git-240.png";
+import PyLogo from "../../assets/img/experience/icons8-python-240.png";
+import DockerLogo from "../../assets/img/experience/icons8-docker-240.png";
 
-export function Tech () {
-    return (
-        <>
-            <section className="skills" id="skills">
-                <h2 className="skill-header">Tech experience</h2>
-                <div className="skills-container">  
-                    <div className = "icon">              
-                        <a href="https://mzl.la/3mD6SFd">
-                            <img
-                                src={HtmlLogo}
-                                alt="HTML"
-                                className="icon-card"
-                            />
-                        </a>
-                    </div>
-                    <div className = "icon">              
-                        <a href="https://mzl.la/3pZdoIl">
-                            <img
-                                src={CssLogo}
-                                alt="CSS"
-                                className="icon-card"
-                            />
-                        </a>
-                    </div>
-                    <div className = "icon">              
-                        <a href="https://mzl.la/3k0nK74"> 
-                            <img
-                                src={JsLogo}
-                                alt="JS"
-                                className="icon-card"
-                            />
-                        </a>
-                    </div>
-                    <div className = "icon">              
-                        <a href="https://bit.ly/3BFg1RU">
-                            <img
-                                src={ReactLogo}
-                                alt="ReactJS"
-                                className="icon-card"
-                            />
-                        </a>
-                    </div>
-                    <div className = "icon">              
-                        <a href="https://bit.ly/3q0aR0t">
-                            <img
-                                src={NodeLogo}
-                                alt="NodeJS"
-                                className="icon-card"
-                            />
-                        </a>
-                    </div>
-                    <div className = "icon">              
-                        <a href="https://bit.ly/3LvE0Jl">
-                            
-                            <img
-                                src={GitLogo}
-                                alt="Git"
-                                className="icon-card"
-                            />
-                        </a>
-                    </div>
-                </div>
-            </section>
-        </>
-    );
+export function Tech() {
+  return (
+    <>
+      <section className="skills" id="skills">
+        <h2 className="skill-header">Tech stack</h2>
+        <div className="skills-container">
+          <div className="icon">
+            <a href="https://bit.ly/3q0aR0t">
+              <img src={NodeLogo} alt="NodeJS" className="icon-card" />
+            </a>
+          </div>
+
+          <div className="icon">
+            <a href="https://mzl.la/3k0nK74">
+              <img src={JsLogo} alt="JS" className="icon-card" />
+            </a>
+          </div>
+
+          <div className="icon">
+            <a href="https://www.python.org/">
+              <img src={PyLogo} alt="Python" className="icon-card" />
+            </a>
+          </div>
+
+          <div className="icon">
+            <a href="https://www.docker.com/">
+              <img src={DockerLogo} alt="Docker" className="icon-card" />
+            </a>
+          </div>
+
+          <div className="icon">
+            <a href="https://bit.ly/3LvE0Jl">
+              <img src={GitLogo} alt="Git" className="icon-card" />
+            </a>
+          </div>
+
+          <div className="icon">
+            <a href="https://bit.ly/3BFg1RU">
+              <img src={ReactLogo} alt="ReactJS" className="icon-card" />
+            </a>
+          </div>
+
+          <div className="icon">
+            <a href="https://mzl.la/3mD6SFd">
+              <img src={HtmlLogo} alt="HTML" className="icon-card" />
+            </a>
+          </div>
+          <div className="icon">
+            <a href="https://mzl.la/3pZdoIl">
+              <img src={CssLogo} alt="CSS" className="icon-card" />
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
